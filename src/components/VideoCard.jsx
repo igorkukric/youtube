da88b79bd5a062ff282c60e1a426bae101e4ed84
Igor_Kukric <igorkukric@gmail.com>
2023-12-22 23:40:44 +0100

    render videos

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Typography, Card, CardContent, CardMeida } from "@mui/material";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
 import {
@@ -10,8 +10,22 @@ import {
   demoChannelTitle,
 } from "../utils/constants";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({video: {id: {
+  videoId
+}, snippet}}) => {
+  console.log(videoId, snippet)
+
+  return (
+      <Card>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <CardMedia 
+              image={snippet?.thumbnails?.high?.url} 
+              alt={snippet?.title}
+              sx={{ width: 358, height: 180}}
+              />
+        </Link>
+      </Card>
+    )
 };
 
 export default VideoCard;
