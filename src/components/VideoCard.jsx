7d0a1c815b166ab5d5c7a65f0d4f22cee127eb26
Igor_Kukric <igorkukric@gmail.com>
2023-12-22 23:30:57 +0100

    render videos, update video card imports

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMeida } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = () => {
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
